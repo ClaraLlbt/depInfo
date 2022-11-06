94930f2fa881bedf1d9ba377b65395e63e179513
Clara hlt <clarahltc@gmail.com>
2022-11-06 13:50:55 +0100

    config files to the  deployement

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/Home",
+      path: "/",
       name: "home",
       component: HomeView,
     }
   ],
+  base: 'https://clarahlt.github.io/depInfo/'
 });
 
 export default router;
